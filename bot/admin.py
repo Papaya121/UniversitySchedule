b08fca823ac2e4c6870a6da983b4b2c134be1018
Vladimir <Bigteamer@yandex.ru>
2026-09-22 14:16:59 +0300

    Updates!!

--- bot/admin.py
+++ bot/admin.py
@@ -63,6 +63,7 @@ def build_admin_router(
             f"📣 Массовых рассылок: <b>{stats['broadcast_count']}</b> "
             f"({stats['broadcast_delivered']} доставлено)\n\n"
             f"<b>Группы:</b>\n{group_lines}"
+            f"\nВерсия 0.2.0"
         )
 
     @router.message(Command("myid"))
